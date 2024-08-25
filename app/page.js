97618740115c2883d6339f3bab9931c0b1bfac10
hanke <jchengyu0829@163.com>
2024-08-25 18:41:09 +0800

    init

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+    return (
+        <div className="header">
+            <div className="container clearfix">
+                <img src="/images/logo.png" alt="图片"/>
+            </div>
+        </div>
+    );
+}
