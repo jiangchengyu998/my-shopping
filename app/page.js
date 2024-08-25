0e0faf34aca4b0fb581de6e0de2f81400b855f71
hanke <jchengyu0829@163.com>
2024-08-25 20:18:52 +0800

    完成 header。

--- app/page.js
+++ app/page.js
@@ -2,7 +2,15 @@ export default function HomePage() {
     return (
         <div className="header">
             <div className="container clearfix">
-                <img src="/images/logo.png" alt="图片"/>
+                <div className="logo leftfix">
+                    <img src="/images/logo.png" alt="尚品汇"/>
+                </div>
+                <div className="search rightfix">
+                    <form action="#">
+                        <input type="text"/>
+                        <button></button>
+                    </form>
+                </div>
             </div>
         </div>
     );
